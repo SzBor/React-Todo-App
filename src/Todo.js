c3857dc271c127ec8bd66407d080273cb42ee432
Szymon Borucki <szymon@MacBook-Pro-Szymon.local>
2019-07-20 02:09:30 +0200

    decomposition of todo component

--- src/Todo.js
+++ src/Todo.js
@@ -22,44 +22,59 @@ const options = [
   }
 ];
 
+const Header = props => (
+  <header className="header">
+    <h1>Todo</h1>
+    {props.children}
+  </header>
+);
+
+const Main = props => (
+  <section className="main">
+    <input id="toggle-all" className="toggle-all" type="checkbox" />
+    <label htmlFor="toggle-all">Mark all as complete</label>
+    <ul className="todo-list">{props.children}</ul>
+  </section>
+);
+
+const Controls = props => (
+  <footer className="footer">
+    <span className="todo-count" />
+    <ul className="filters">
+      {options.map(option => (
+        <li>
+          <a
+            href={`#/${option.href}`}
+            className={option.isActive ? "selected" : ""}
+          >
+            {option.label}
+          </a>
+        </li>
+      ))}
+    </ul>
+    <button className="clear-completed">Clear completed</button>
+  </footer>
+);
+
 class Todo extends Component {
   render() {
     return (
       <div>
         <section className="todoapp">
-          <header className="header">
-            <h1>Todos</h1>
+          <Header>
             <AddTaskForm />
-          </header>
-          <section className="main">
-            <input id="toggle-all" className="toggle-all" type="checkbox" />
-            <label htmlFor="toggle-all">Mark all as complete</label>
-            <ul className="todo-list">
-              {data.tasks.map(task => (
-                <TaskListItem
-                  isEditing={false}
-                  taskTitle={task.title}
-                  isDone={task.isDone}
-                />
-              ))}
-            </ul>
-          </section>
-          <footer className="footer">
-            <span className="todo-count" />
-            <ul className="filters">
-              {options.map(option => (
-                <li>
-                  <a
-                    href={`#/${option.href}`}
-                    className={option.isActive ? "selected" : ""}
-                  >
-                    {option.label}
-                  </a>
-                </li>
-              ))}
-            </ul>
-            <button className="clear-completed">Clear completed</button>
-          </footer>
+          </Header>
+          <Main>
+            {data.tasks.map(task => (
+              <TaskListItem
+                isEditing={false}
+                taskTitle={task.title}
+                isDone={task.isDone}
+              />
+            ))}
+          </Main>
+
+          <Controls />
         </section>
         <Footer />
       </div>
