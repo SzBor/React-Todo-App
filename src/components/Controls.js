9cabb41fafeecc192dde26351f3af19eb834ce5d
Szymon Borucki <szymonborucki@op.pl>
2019-07-23 02:24:15 +0200

    changed Controls component into clsss

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,40 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class Controls extends Component {
+  state = {
+    options: [
+      {
+        href: '',
+        isActive: true,
+        labe: 'All'
+      },
+      {
+        href: 'active',
+        label: 'Active'
+      },
+      {
+        href: 'completed',
+        label: 'Completed'
+      }
+    ]
+  };
 
-export const options = [
-    {
-      href: '',
-      isActive: true,
-      labe: 'All'
-    },
-    {
-      href: 'active',
-      label: 'Active'
-    },
-    {
-      href: 'completed',
-      label: 'Completed'
-    }
-  ];
+  render() {
+    return (
+      <footer className="footer">
+        <span className="todo-count" />
+        <ul className="filters">
+          {this.state.options.map(option => (
+            <li key={option.href}>
+              <a
+                href={`#/${option.href}`}
+                className={option.isActive ? 'selected' : ''}
+              >
+                {option.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <button className="clear-completed">Clear completed</button>
+      </footer>
+    );
+  }
+}
 
-  const Controls = props => (
-    <footer className="footer">
-      <span className="todo-count" />
-      <ul className="filters">
-        {options.map(option => (
-          <li key={option.href}>
-            <a
-              href={`#/${option.href}`}
-              className={option.isActive ? 'selected' : ''}
-            >
-              {option.label}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <button className="clear-completed">Clear completed</button>
-    </footer>
-  );
-
-
-  export default Controls;
+export default Controls;
